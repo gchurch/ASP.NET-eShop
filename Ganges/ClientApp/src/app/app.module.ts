3753d7bf9de296b883bab0e526abc3a627c23617
George Church <georgechurch10@gmail.com>
2020-07-09 14:46:58 +0100

    Removed the home page component. The root url is now redirected to the products page.

--- Ganges/ClientApp/src/app/app.module.ts
+++ Ganges/ClientApp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
 
@@ -17,7 +16,6 @@ import { AddProductComponent } from './add-product/add-product.component';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
     ProductsComponent,
     ProductComponent,
     AddProductComponent
@@ -27,7 +25,7 @@ import { AddProductComponent } from './add-product/add-product.component';
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'products', pathMatch: 'full' },
       { path: 'products', component: ProductsComponent },
       { path: 'products/:id', component: ProductComponent },
       { path: 'add-product', component: AddProductComponent }
