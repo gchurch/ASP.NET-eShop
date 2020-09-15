3c35385784837fc39ec73e08f3ebeee7890048bb
George Church <georgechurch10@gmail.com>
2020-09-15 12:25:43 +0100

    Created first simple end-to-end function test for the Web.MVC project.

--- tests/FunctionalTests/Web.MVC/Tests.cs
+++ tests/FunctionalTests/Web.MVC/Tests.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Ganges.Web.MVC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Ganges.FunctionalTests.Web.MVC
 {
     [TestClass]
-    class Tests
+    public class Tests
     {
+
+        [DataTestMethod]
+        [DataRow("Products")]
+        [DataRow("Products/Create")]
+        [DataRow("Products/Details/1")]
+        [DataRow("Products/Edit/1")]
+        [DataRow("Products/Delete/1")]
+        public async Task Get_EndpointsReturnSuccess(string url)
+        {
+            // Arrange
+            CustomWebApplicationFactory<Startup> factory
+                = new CustomWebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
