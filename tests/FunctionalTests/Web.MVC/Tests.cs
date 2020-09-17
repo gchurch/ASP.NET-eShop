8631700d828fb7d2aec35c3c78adca5e167535d3
George Church <georgechurch10@gmail.com>
2020-09-17 16:54:20 +0100

    Added a function test for the Web.MVC project to test that adding a product using a post requests results in a successful get request of the added product.

--- tests/FunctionalTests/Web.MVC/Tests.cs
+++ tests/FunctionalTests/Web.MVC/Tests.cs
@@ -1,7 +1,12 @@
-﻿using Ganges.Web.MVC;
+﻿using Ganges.ApplicationCore.Entities;
+using Ganges.Web.MVC;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,6 +35,44 @@ namespace Ganges.FunctionalTests.Web.MVC
 
             // Assert
             response.EnsureSuccessStatusCode();
+        }
+
+        [TestMethod]
+        public async Task PostingNewProductShouldResultInSuccessfulGetRequest()
+        {
+            // Arrange
+            CustomWebApplicationFactory<Startup> factory
+                = new CustomWebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+            var product = new Product()
+            {
+                Title = "TestTitle"
+            };
+            var productString = JsonConvert.SerializeObject(product);
+            var stringContent = new StringContent(productString, Encoding.UTF8, "application/json");
+
+            // Act
+            var postResponse = await client.PostAsync("/Products/Create", stringContent);
+
+            var getResponse = await client.GetAsync("/Products/Details/4");
+
+            // Assert
+            getResponse.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        [TestMethod]
+        public async Task NotFoundTest()
+        {
+            // Arrange
+            CustomWebApplicationFactory<Startup> factory
+                = new CustomWebApplicationFactory<Startup>();
+            var client = factory.CreateClient();
+
+            // Act
+            var getResponse = await client.GetAsync("/Products/Details/4");
+
+            // Assert
+            getResponse.StatusCode.ShouldBe(HttpStatusCode.NotFound);
         }
     }
 }
