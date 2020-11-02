74a9accbb2ac81ac4024b7200d11fd5384e0db72
George Church <georgechurch10@gmail.com>
2020-11-02 15:38:22 +0000

    Added Required data annotations to the Product class.

--- src/ApplicationCore/Entities/Product.cs
+++ src/ApplicationCore/Entities/Product.cs
@@ -1,12 +1,19 @@
-﻿namespace ApplicationCore.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationCore.Entities
 {
     public class Product
     {
         public int Id { get; set; } 
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Seller { get; set; }
+        [Required]
         public int Price { get; set; }
+        [Required]
         public int Quantity { get; set; }
         public string ImageUrl { get; set; }
 
