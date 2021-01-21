2dc4326434007f0511a5f724b92d736720f94221
George Church <georgechurch10@gmail.com>
2021-01-21 16:41:57 +0000

    [React] The product component can now request the product data for the product id given in the url.

--- src/Web.React/ClientApp/src/components/Product.js
+++ src/Web.React/ClientApp/src/components/Product.js
@@ -1,30 +1,42 @@
 import React, {Component} from 'react';
-import { useParams } from 'react-router-dom';
 
 export class Product extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            product: null,
-            loading: true
-        };
+        this.state = { product: {}, loading: true };
+    }
+
+    componentDidMount() {
+        this.populateProductData();
+    }
+
+    async populateProductData() {
+        const id = this.props.match.params.id;
+        const response = await fetch('api/products/' + id);
+        const data = await response.json();
+        this.setState({ product: data, loading: false });
     }
 
     render () {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Product.renderProduct(this.state.product);
+
         return (
-            <Body/>
+            <div>
+                {contents}
+            </div>
         );
     }
-}
 
-function Body() {
-    let { id } = useParams();
+    static renderProduct (product) {
+        return (
+            <div>
+                <h1>Title: {product.title}</h1>
+                <p>ID: {product.productId}</p>
+            </div>
+        );
+    }
 
-    return (
-        <div>
-            <h1>Product</h1>
-            <p>ID: {id}</p>
-        </div>
-    );
 }
