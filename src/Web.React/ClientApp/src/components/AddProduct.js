e2854ee9b4c09837b6251da2f32c4a2dbaaa5ec7
George Church <georgechurch10@gmail.com>
2021-01-23 16:33:07 +0000

    [React] Created form in the AddProduct component to create a new product. The product is not yet sent to the server.

--- src/Web.React/ClientApp/src/components/AddProduct.js
+++ src/Web.React/ClientApp/src/components/AddProduct.js
@@ -1,9 +1,68 @@
 import React, {Component} from 'react';
 
 export class AddProduct extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            description: "",
+            seller: "",
+            price: 0,
+            quantity: 0,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value 
+        });
+    }
+
+    handleSubmit(event) {
+        alert('New product created: ' + this.state.title);
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <h1>Add a product!</h1>
+            <div>
+                <h1>Add a Product</h1>
+                <form onSubmit={this.handleSubmit}>
+
+                    <div>
+                        <p>Product Title: <input name="title" type="text" value={this.state.title} onChange={this.handleChange} /></p>
+                    </div>
+    
+                    <div>
+                        <p>Price: £<input name="price" type="number" value={this.state.price} onChange={this.handleChange} /></p>
+                    </div>
+
+                    <div>
+                        <p>Quantity: <input name="quantity" type="number" value={this.state.quantity} onChange={this.handleChange} /></p>
+                    </div>
+
+                    <div>
+                        <p>Description: <input name="description" type="text" value={this.state.description} onChange={this.handleChange} /></p>
+                    </div>
+
+                    <div>
+                        <p>Seller's Name: <input name="seller" type="text" value={this.state.seller} onChange={this.handleChange} /></p>
+                    </div>
+
+                    <input type="submit" value="Submit" />
+
+                </form>
+            </div>
         );
     }
 }
