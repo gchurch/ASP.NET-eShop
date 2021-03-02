732f65ea1cc61a67e40082a1bbb3e039f4f306b2
George Church <georgechurch10@gmail.com>
2021-03-02 17:00:05 +0000

    [React] Changed the startup swagger configuration so that it uses the ClassLibrary xml file and thus gets the API documentation.

--- src/Web.React/Startup.cs
+++ src/Web.React/Startup.cs
@@ -40,7 +40,10 @@ namespace Web.React
                 });
 
                 // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                var xmlFile = $"ClassLibrary.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
