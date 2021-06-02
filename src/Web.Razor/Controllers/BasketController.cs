5241003315283ddb44b327875f355ac52635a436
George Church <15802979+gchurch@users.noreply.github.com>
2021-06-02 12:23:40 +0100

    [Razor] Added button to the Details page that adds that product to the basket. All seems to be working.

--- src/Web.Razor/Controllers/BasketController.cs
+++ src/Web.Razor/Controllers/BasketController.cs
@@ -13,25 +13,46 @@ namespace Web.Razor.Controllers
     public class BasketController : Controller
     {
         private readonly IBasketService _basketService;
+        private readonly IProductService _productService;
         private readonly IAuthorizationService _authorizationService;
         private readonly UserManager<IdentityUser> _userManager;
 
         public BasketController(
             IBasketService basketService,
+            IProductService productService,
             IAuthorizationService authorizationService,
             UserManager<IdentityUser> userManager
         )
         {
             _basketService = basketService;
+            _productService = productService;
             _authorizationService = authorizationService;
             _userManager = userManager;
         }
 
+        // GET: /Basket
         public IActionResult Index()
         {
             string userId = _userManager.GetUserId(User);
             Basket basket = _basketService.GetBasket(userId);
             return View(basket);
+        }
+
+        // GET: /Basket/Add/1
+        [HttpGet]
+        public async Task<ActionResult> Add(int id)
+        {
+            Console.WriteLine("Adding product with ID: " + id + " to basket");
+            if (await _productService.DoesProductIdExist(id))
+            {
+                string userId = _userManager.GetUserId(User);
+                _basketService.AddProductToBasket(id, userId);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
--- src/Web.Razor/wwwroot/js/site.js
+++ src/Web.Razor/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function AddProductToBasket(productId) {
+    console.log("Sending request to add product to the server.");
+    fetch('/Basket/Add/' + productId)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
